5b23152cbbd9d51c31b628e4c7abde0e8e38ee9b
MathiasVeraM <mathias.vera@udla.edu.ec>
2023-07-09 23:37:10 -0500

    Se agrego la funcion de borrar los datos del paciente que se requiera

--- Historialmedico.c
+++ Historialmedico.c
@@ -58,3 +58,48 @@ void buscarDatos(char nombreBuscado[]){
     }
     fclose(archivo);
 }
+
+void borrarDatos(char nombreBorrar[]){ //Esta funcion es para borrar los datos de un paciente en especifico
+    FILE *archivoEntrada;
+    archivoEntrada = fopen("datos_pacientes.csv", "r");
+
+    if (archivoEntrada == NULL) {
+        printf("El archivo no existe o no se puede abrir.\n");
+        return;
+    }
+
+    FILE *archivoTemporal;
+    archivoTemporal = fopen("temp.txt", "w");
+
+    if (archivoTemporal == NULL) {
+        printf("Error al crear el archivo temporal.\n");
+        fclose(archivoEntrada);
+        return;
+    }
+
+    struct Paciente paciente;
+    int encontrado = 0;
+
+    while (fscanf(archivoEntrada, "%s %d %f %f %f %d %f %d\n", paciente.nombre, &paciente.edad, &paciente.altura, &paciente.peso, &paciente.presionArterial,
+    &paciente.pulso, &paciente.temperatura, &paciente.frecuenciaRespiratoria, &paciente.imc) != EOF) {
+        if (strcmp(paciente.nombre, nombreBorrar) != 0) {
+            fprintf(archivoTemporal, "%s %d %.2f %.2f %.2f %d %.2f %d\n", paciente.nombre, &paciente.edad, &paciente.altura, &paciente.peso, &paciente.presionArterial,
+    &paciente.pulso, &paciente.temperatura, &paciente.frecuenciaRespiratoria, &paciente.imc);
+        } else {
+            encontrado = 1;
+        }
+    }
+
+    fclose(archivoEntrada);
+    fclose(archivoTemporal);
+
+    if (!encontrado) {
+        printf("Paciente no encontrado.\n");
+        remove("temp.txt");
+        return;
+    }
+
+    remove("datos_pacientes.txt");
+    rename("temp.txt","datos_pacientes.txt");
+    printf("Paciente borrado exitosamente.\n");
+}
